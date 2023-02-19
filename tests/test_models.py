873cdfa06d6e090b1f41bcb938cc9214d8de08f0
Tim D. Smith <git@tim-smith.us>
2023-02-18 16:17:02 -0800

    FIx models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,2 +1,8 @@
-def test_import():
-    import sdif.models
+import sdif.fields as fields
+import sdif.model_meta as model_meta
+
+
+def test_m1_m2_exclusive():
+    for cls in model_meta.REGISTERED_MODELS.values():
+        for field in fields.record_fields(cls):
+            assert not (field.m1 and field.m2)
