6afdddc0cc62d51cfd9eaefc5df136f69fb7c3e9
Tim D. Smith <git@tim-smith.us>
2023-02-18 16:12:55 -0800

    Add exports

--- src/sdif/__init__.py
+++ src/sdif/__init__.py
@@ -2,3 +2,10 @@ import sdif.fields as fields
 import sdif.models as models
 import sdif.records as records
 import sdif.time as time
+
+__all__ = [
+    "fields",
+    "models",
+    "records",
+    "time",
+]
